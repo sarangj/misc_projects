8f284e61f147a72b49042a05495bae65a1618f5e
Andrew R. Mooney <mooney@fb.com>
2016-01-01 11:55:12 -0600

    minor change

--- squawk/config.py
+++
@@ -1,8 +0,0 @@
-import os
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(PROJECT_ROOT, 'app', 'static', 'words.db')
-DEBUG = True
-SECRET_KEY = 'devkey'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
