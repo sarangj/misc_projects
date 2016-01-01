de7f8983d66c20ce704690cdef962e59e008888d
Andrew R. Mooney <mooney@fb.com>
2016-01-01 11:58:43 -0600

    removing db func

--- squawk/runsite.py
+++ squawk/runsite.py
@@ -10,12 +10,6 @@ app.config.from_object('config')
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-#def init_db():
-#    with closing(connect_db()) as db:
-#        with app.open_resource('schema.sql', mode='r') as f:
-#            db.cursor().executescript(f.read())
-#        db.commit()
-
 @app.before_request
 def before_request():
     g.db = connect_db()
