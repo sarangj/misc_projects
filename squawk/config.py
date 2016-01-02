8aa63efbe2849100d0bc303f028abf08e5a62054
Andrew R. Mooney <mooney@fb.com>
2016-01-02 09:50:55 -0600

    mod gitignore

--- squawk/config.py
+++
@@ -1,8 +0,0 @@
-import os
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(PROJECT_ROOT, 'app', 'static', 'words.db')
-DEBUG = False
-SECRET_KEY = 'oooooohbigbigbigsecretdevkey'
-USERNAME = 'admin'
-PASSWORD = 'bigbigbigpassword'
-
